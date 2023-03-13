ce8a62c1ad8c360072d614692d7d5c734f72cf52
sterenn-dubois <sterenn.dubois@laplateforme.io>
2023-03-13 14:32:18 +0100

    ajout

---
+++ Jour6/Password.py
@@ -0,0 +1,6 @@
+#Must contain 8 carac, 1 maj letter, 1 maj min, 1 number, 1special carac (!,@,#,$,%,^,&,*)
+#user must choose password
+#must respect exig security
+#if pass valid, show a confirm message and quit prog
+#if not valid, show an error mess and ask user to choose a new password
+#repeat step 2 and 4 until user choose a valid password
